5f19979ee137f29eef1772bf055e40e0191211df
quitgame <iuryuplay@gmail.com>
2020-11-02 21:22:43 -0300

    https

---
+++ desafio.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(function(req, res, next){ 
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) 
+        res.redirect(`https://${req.headers.host}${req.url}`); 
+    else
+        next(); 
+});
+
+app.get("/",function(req,res){
+    res.send('hello world')
+})
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('funfando');
+});
+
+
